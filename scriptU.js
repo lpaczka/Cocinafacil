750061b506e3996bf49067ec35dfcbf6c3fffdfe
wbrianw <wbrianw_98@hotmail.com>
2018-12-14 17:04:46 -0600

    linkear botones de paquetes

---
+++ scriptU.js
@@ -0,0 +1,108 @@
+'use strict'
+//----------------PAQUETES---------------------
+//ID paqutes Familiares : PF1, PF2, PF3, PF4
+//ID paquetes Gym : PG1, PG2, PG3, PG4
+//ID paquetes Godin : PGo1, PGo2, PGo3, PGo4
+//ID paqutes Estudiante : PE1, PE2, PE3, PE4
+//ID descripcion de paquetes Familiares : PFD1, PFD2, PFD3, PFD4
+//ID descripcion de paquetes Gym : PGD1, PGD2, PGD3, PGD4
+//ID descripcion de paquetes Godin : PGoD1, PGoD2, PGoD3, PGoD4
+//ID descripcion de paoquetes Estudiante : PED1, PED2, PED3, PED4
+//----------------CHECK---------------------
+//ID Nombre de paquete en check : NomPaquete
+//ID Descripcion paquete en check : DescPaquete
+//---------------BOTONES----------------------
+//ID para botones en Paquete Familiar : BF1, BF2, BF3, BF4
+//ID para botones en Paquete Gym : BG1, BG2, BG3, BG4
+//ID para botGnes Gn PaGueteGGodin : BGo1, BGo2, BGo3, BGo4
+//ID paraGGootonGGo en GGoquetGGoEstudiante : BE1, BE2, BE3, BE4
+
+//Variables de Nombres de Paquetes Familiares
+let PF1 = document.getElementById('PF1');
+let PF2 = document.getElementById('PF2');
+let PF3 = document.getElementById('PF3');
+let PF4 = document.getElementById('PF4');
+
+//Variables de Nombres de Paqutes Gym
+let PG1 = document.getElementById('PG1');
+let PG2 = document.getElementById('PG2');
+let PG3 = document.getElementById('PG3');
+let PG4 = document.getElementById('PG4');
+
+//Variables de Nombres de Paqutes Godin
+let PGo1 = document.getElementById('PGo1');
+let PGo2 = document.getElementById('PGo2');
+let PGo3 = document.getElementById('PGo3');
+let PGo4 = document.getElementById('PGo4');
+
+//Variables de Nombres de Paqutes Estudiante
+let PE1 = document.getElementById('PE1');
+let PE2 = document.getElementById('PE2');
+let PE3 = document.getElementById('PE3');
+let PE4 = document.getElementById('PE4');
+
+//Variables de Descripcion de Paqutes Familiares
+let PFD1 = document.getElementById('PFD1');
+let PFD2 = document.getElementById('PFD2');
+let PFD3 = document.getElementById('PFD3');
+let PFD4 = document.getElementById('PFD4');
+
+//Variables de Descripcion de Paqutes Gym
+let PGD1 = document.getElementById('PGD1');
+let PGD2 = document.getElementById('PGD2');
+let PGD3 = document.getElementById('PGD3');
+let PGD4 = document.getElementById('PGD4');
+
+//Variables de Descripcion de Paqutes Godin
+let PGoD1 = document.getElementById('PGoD1');
+let PGoD2 = document.getElementById('PGoD2');
+let PGoD3 = document.getElementById('PGoD3');
+let PGoD4 = document.getElementById('PGoD4');
+
+//Variables de Descripcion de Paqutes Estudiantes
+let PED1 = document.getElementById('PED1');
+let PED2 = document.getElementById('PED2');
+let PED3 = document.getElementById('PED3');
+let PED4 = document.getElementById('PED4');
+
+//Variables de Nombre en Check
+let name = document.getElementById('NomPaquete');
+
+//Variables de Descripcion  en Check
+let descripcion = document.getElementById('DescPaquete');
+
+//Variables de Boton Familiar
+let BF1 = document.getElementById('BF1');
+let BF2 = document.getElementById('BF2');
+let BF3 = document.getElementById('BF3');
+let BF4 = document.getElementById('BF4');
+
+//Variables de Boton Gym
+let BG1 = document.getElementById('BG1');
+let BG2 = document.getElementById('BG2');
+let BG3 = document.getElementById('BG3');
+let BG4 = document.getElementById('BG4');
+
+//Variables de Boton Godin
+let BGo1 = document.getElementById('BGo1');
+let BGo2 = document.getElementById('BGo2');
+let BGo3 = document.getElementById('BGo3');
+let BGo4 = document.getElementById('BGo4');
+
+//Variables de Boton Estudiante
+let BE1 = document.getElementById('BE1');
+let BE2 = document.getElementById('BE2');
+let BE3 = document.getElementById('BE3');
+let BE4 = document.getElementById('BE4');
+
+// name.innerrHTML = PF1;
+
+// name.innerHTML = descripcion.innerHTML;
+
+// const dec1 = () => {
+//     name.innerHTML = 'Hola';
+//     // descripcion = PFD1.value;
+// }
+
+// BF1.addEventListener('click', dec1)
+
